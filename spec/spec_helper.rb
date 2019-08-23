8dc45dcf55283c563416541ba580c130dcf47061
Amritpal Maan <sonny_maan@yahoo.com>
2019-08-23 11:45:34 +0100

    Set up simplecov dependancies and edited spec helper to show more info while testing, also added color and format documentation to .rspec file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,10 +8,19 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'simplecov'
+require 'simplecov-console'
 
 # tell Capybara about our app class
 Capybara.app = Battle
 
+#simple cov formatting
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+                                                                 SimpleCov::Formatter::Console,
+                                                                 # Want a nice code coverage website? Uncomment this next line!
+                                                                 # SimpleCov::Formatter::HTMLFormatter
+                                                               ])
+SimpleCov.start
 
 
 
