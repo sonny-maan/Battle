1dfc41736b0761f3bab66b041a6c97145630b4c6
Amritpal Maan <sonny_maan@yahoo.com>
2019-08-23 12:21:10 +0100

    Added the play.erb view for after the enter names form is submitted, accessed names as instance variable through params array

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    @name_p1 = params[:name_p1]
+    @name_p2 = params[:name_p2]
+    erb :play
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
